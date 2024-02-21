ce9ce4f45744889cbf215ef35705de796a434d64
Hossein <rezaeig22@gmail.com>
2024-02-21 17:59:30 +0330

    Build AuthPage

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import OTPInput from '@/components/OTPInput';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
+
+interface Data {
+  otp: string;
+  number?: string;
+}
+
+const AuthPage = () => {
+  const handleComplete = (data: Data) => {
+    axios
+      .post('/api/user/signup', data)
+      .then((res) => toast.success(res.data.message))
+      .catch((err) => toast.error(err.response.data));
+  };
+
+  return (
+    <div className="w-80 m-auto">
+      <Toaster />
+      <OTPInput onSubmit={(otpValue) => handleComplete(otpValue)} />
+    </div>
+  );
+};
+
+export default AuthPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,5 @@
-'use client';
-
-import OTPInput from '@/components/OTPInput';
-import axios from 'axios';
-import toast, { Toaster } from 'react-hot-toast';
-
-interface Data {
-  otp: string;
-  number?: string;
-}
-
 const HomePage = () => {
-  const handleComplete = (data: Data) => {
-    axios
-      .post('/api/user/signup', data)
-      .then((res) => toast.success(res.data.message))
-      .catch((err) => toast.error(err.response.data));
-  };
-
-  return (
-    <div className="w-80 m-auto">
-      <Toaster />
-      <OTPInput onSubmit={(otpValue) => handleComplete(otpValue)} />
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
